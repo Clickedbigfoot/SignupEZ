b602bc01f4f977595dfc31288ab11738729523f1
Clicked <clickedbigfoot@gmail.com>
2020-05-02 23:05:03 -0500

    Added main file and todo list

---
+++ main.cs
@@ -0,0 +1,14 @@
+/*
+This is a signup application in C#
+@TODO
+1) Read in data
+2) Open web browser
+3) Perform goal of script
+4) Implement safeguard to make program expire
+*/
+
+using System;
+
+static void Main() {
+	Console.WriteLine("Hello world!")
+}
