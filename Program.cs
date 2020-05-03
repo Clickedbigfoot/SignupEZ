6a6715eff0bcea531d2bde3d4c5e03e0956f6ff5
Clickedbigfoot <clickedbigfoot@gmail.com>
2020-05-03 16:56:01 -0500

    Finished reading in and checking read data

--- Program.cs
+++ Program.cs
@@ -1,22 +1,43 @@
 ﻿/*
-Signup Script
+Signup Script. Reads in the user's net ID, password, and the CRNs of the target classes and the classes to drop if a spot opens.
 @TODO
-1) Make this read in data
+1) DONE Make this read in data
 2) Open a web browser
 3) Perform goal
 4) Set expiry code
 5) Compile for windows and export
 */
 
-using System;
-
 namespace SignupEZ
 {
     class Program
     {
+    	readonly static string CREDENTIALS_LOCATION = "loginCredentials.txt";
+    	readonly static string TARGET_CLASSES_LOCATION = "targetCRN.txt";
+    	readonly static string DROP_CLASSES_LOCATION = "dropCRN.txt";
+    	readonly static string errorCredentials = @"Error: Ensure your netID is in line one of loginCredentials.txt and your password is in line two";
+    	readonly static string errorTargetCRN = @"Error: Ensure there is at least one CRN in targetCRN.txt";
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            System.Console.WriteLine("Hello World!");
+            string[] credentials = System.IO.File.ReadAllLines(CREDENTIALS_LOCATION); //[0] is the netID while [1] is the password
+            string[] targets = System.IO.File.ReadAllLines(TARGET_CLASSES_LOCATION); //Array of each target class' CRN
+            string[] drops = System.IO.File.ReadAllLines(DROP_CLASSES_LOCATION); //Array of each class' CRN to drop
+
+            //Check for issues
+            if (credentials.Length < 2) {
+            	System.Console.WriteLine(errorCredentials);
+            	return;
+            }
+            else if (targets.Length < 1) {
+            	System.Console.WriteLine(errorTargetCRN);
+            	return;
+            }
+
+            Signup test = new Signup();
+            
         }
     }
 }
---
+++ Signup.cs
@@ -0,0 +1,20 @@
+/*
+Signup script
+Checks whether or not there is a spot open and signs up the user automatically if there is.
+Drops any course necessary as specified by the user.
+*/
+
+
+namespace SignupEZ {
+
+	class Signup {
+		public void test() {
+			System.Console.WriteLine("Class importing worked!");
+		}
+	}
+
+
+
+
+
+}
