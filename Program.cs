795aa15e249c3f5dc5e780cc514b90eaae0c324c
Clickedbigfoot <clickedbigfoot@gmail.com>
2020-05-02 23:39:15 -0500

    Switched to c#

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+Signup Script
+@TODO
+1) Make this read in data
+2) Open a web browser
+3) Perform goal
+4) Set expiry code
+5) Compile for windows and export
+*/
+
+using System;
+
+namespace SignupEZ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
