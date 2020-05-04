fdf4ee2d780eafd442893cabf0960e36ee670205
Clickedbigfoot <clickedbigfoot@gmail.com>
2020-05-03 21:18:05 -0500

    Now signs in correctly with netid and password

--- Signup.cs
+++ Signup.cs
@@ -11,17 +11,19 @@ namespace SignupEZ {
 
 	class Signup {
 
-		string netID;
+		string netId;
 		string password;
 		string[] targets;
 		string[] drops;
 		FirefoxDriver driver;
 		FirefoxDriverService service;
-		readonly int SLEEP_TIME = 10 * 1000; //n * 1000 == n seconds of time to wait inbetween tasks
-		readonly int SMALL_SLEEP_TIME = 5 * 1000;
+		IWebElement element1;
+		readonly int LARGE_SLEEP_TIME = 10 * 1000; //n * 1000 == n seconds of time to wait inbetween tasks
+		readonly int SLEEP_TIME = 5 * 1000;
+		readonly int SMALL_SLEEP_TIME = 2 * 1000;
 
-		public  Signup(string inputNetID, string inputPassword, string[] inputTargets, string[] inputDrops) {
-			netID = inputNetID;
+		public  Signup(string inputNetId, string inputPassword, string[] inputTargets, string[] inputDrops) {
+			netId = inputNetId;
 			password = inputPassword;
 			targets = inputTargets;
 			drops = inputDrops;
@@ -33,7 +35,16 @@ namespace SignupEZ {
 		public void getLoggedIn() {
 			service = FirefoxDriverService.CreateDefaultService("src", "geckodriver");
 			driver = new FirefoxDriver(service);
-			driver.Navigate().GoToUrl("https://courses.illinois.edu/");
+			driver.Navigate().GoToUrl("https://webprod.admin.uillinois.edu/ssa/servlet/SelfServiceLogin?appName=edu.uillinois.aits.SelfServiceLogin&dad=BANPROD1&target=G");
+			System.Threading.Thread.Sleep(SLEEP_TIME); //Now at login screen
+			while (isPresent("netid")) {
+				driver.FindElementById("netid").SendKeys(netId);
+				System.Threading.Thread.Sleep(SMALL_SLEEP_TIME);
+				driver.FindElementById("easpass").SendKeys(password);
+				System.Threading.Thread.Sleep(SMALL_SLEEP_TIME);
+				driver.FindElementByName("BTN_LOGIN").Click();
+				System.Threading.Thread.Sleep(LARGE_SLEEP_TIME); //Should be at screen for selecting course registration
+			}
 
 			System.Console.WriteLine("Setup complete");
 			System.Threading.Thread.Sleep(SLEEP_TIME);
@@ -44,7 +55,7 @@ namespace SignupEZ {
 		*/
 		public void refresh() {
 			this.finish();
-			System.Threading.Thread.Sleep(SMALL_SLEEP_TIME);
+			System.Threading.Thread.Sleep(SLEEP_TIME);
 			this.getLoggedIn();
 		}
 
@@ -55,10 +66,20 @@ namespace SignupEZ {
 			System.Console.WriteLine("Exiting");
 			driver.Close();
 		}
+
+		/*
+		Determines if an element is present by its ID
+		@param elementId: the id of the element being looked for
+		@return true if the element is present, otherwise false
+		*/
+		private bool isPresent(string elementId) {
+			try {
+				element1 = driver.FindElementById(elementId);
+				return true;
+			}
+			catch (System.Exception e) {
+				return false;
+			}
+		}
 	}
-
-
-
-
-
 }
